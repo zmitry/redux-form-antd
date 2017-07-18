997cf4e8422a1e248f3d7c478c07ccd6108767ad
Dmitry Zherebko <kraken@live.ru>
2017-07-19 00:07:15 +0300

    bump version

--- es/components/DatePicker.js
+++ es/components/DatePicker.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.MonthPickerField = exports.DatePickerField = undefined;
+exports.MonthPickerField = exports.DatePickerField = exports.DatePickerFieldRU = undefined;
 
 var _moment = require("moment");
 
@@ -24,17 +24,41 @@ var MonthPicker = _antd.DatePicker.MonthPicker;
 var datePickerMap = (0, _mapError.customMap)(function (_ref) {
   var _ref$input = _ref.input,
       _onChange = _ref$input.onChange,
-      value = _ref$input.value;
+      value = _ref$input.value,
+      dateFormat = _ref.dateFormat;
 
   if (value === "") {
-    value = (0, _moment2.default)();
+    value = "";
   } else {
-    value = (0, _moment2.default)(value);
+    value = (0, _moment2.default)(value, dateFormat);
   }
   return { onChange: function onChange(e, v) {
       return _onChange(v);
-    }, value: value };
+    }, value: value, format: dateFormat };
 });
+
+var datePickerMapRU = (0, _mapError.customMap)(function (_ref2) {
+  var _ref2$input = _ref2.input,
+      _onChange2 = _ref2$input.onChange,
+      value = _ref2$input.value,
+      displayFormat = _ref2.displayFormat,
+      valueFormat = _ref2.valueFormat;
+
+  if (value === "") {
+    value = "";
+  } else {
+    value = (0, _moment2.default)(value);
+  }
+  return {
+    onChange: function onChange(e, v) {
+      _onChange2(e.format(valueFormat));
+    },
+    value: value,
+    format: displayFormat
+  };
+});
+
+var DatePickerFieldRU = exports.DatePickerFieldRU = (0, _BaseComponent2.default)(_antd.DatePicker, datePickerMapRU);
 
 var DatePickerField = exports.DatePickerField = (0, _BaseComponent2.default)(_antd.DatePicker, datePickerMap);
 var MonthPickerField = exports.MonthPickerField = (0, _BaseComponent2.default)(MonthPicker, datePickerMap);
--- es/components/MultiSelect.js
+++ es/components/MultiSelect.js
@@ -7,13 +7,11 @@ exports.SelectField = exports.RadioField = undefined;
 
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 var _react = require("react");
 
 var _react2 = _interopRequireDefault(_react);
-
-var _select = require("antd/lib/select");
-
-var _select2 = _interopRequireDefault(_select);
 
 var _antd = require("antd");
 
@@ -21,40 +19,81 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
 
-var Option = _select2.default.Option;
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var Option = _antd.Select.Option;
 var RadioGroup = _antd.Radio.Group;
 var RadioButton = _antd.Radio.Button;
+var getEmptyArr = function getEmptyArr() {};
 
 var withOptions = function withOptions(OptionType, getType) {
   return function (Component) {
-    return function (_ref) {
-      var options = _ref.options,
-          props = _objectWithoutProperties(_ref, ["options"]);
+    var _class, _temp;
 
-      if (getType) {
-        OptionType = getType(props);
+    return _temp = _class = function (_React$PureComponent) {
+      _inherits(_class, _React$PureComponent);
+
+      function _class() {
+        _classCallCheck(this, _class);
+
+        return _possibleConstructorReturn(this, (_class.__proto__ || Object.getPrototypeOf(_class)).apply(this, arguments));
       }
-      return _react2.default.createElement(
-        Component,
-        props,
-        options.map(function (_ref2, key) {
-          var label = _ref2.label,
-              value = _ref2.value,
-              rest = _objectWithoutProperties(_ref2, ["label", "value"]);
+
+      _createClass(_class, [{
+        key: "render",
+        value: function render() {
+          var _this2 = this;
+
+          var props = this.props;
+
+          if (getType) {
+            OptionType = getType(props);
+          }
+          var valueKey = props.valueKey;
+          var labelKey = props.labelKey;
+          var optionsKey = props.optionsKey;
+          var options = props[optionsKey] || getEmptyArr();
 
           return _react2.default.createElement(
-            OptionType,
-            _extends({}, rest, { key: key, value: value }),
-            label
+            "div",
+            null,
+            _react2.default.createElement("div", { ref: "container" }),
+            _react2.default.createElement(
+              Component,
+              _extends({ getPopupContainer: function getPopupContainer() {
+                  return _this2.refs.container;
+                } }, props),
+              options.map(function (_ref, key) {
+                var value = _ref[valueKey],
+                    label = _ref[labelKey],
+                    rest = _objectWithoutProperties(_ref, [valueKey, labelKey]);
+
+                return _react2.default.createElement(
+                  OptionType,
+                  _extends({}, rest, { key: key, value: String(value) }),
+                  label
+                );
+              })
+            )
           );
-        })
-      );
-    };
+        }
+      }]);
+
+      return _class;
+    }(_react2.default.PureComponent), _class.defaultProps = {
+      valueKey: "value",
+      labelKey: "label",
+      optionsKey: "options"
+    }, _temp;
   };
 };
 
-var RadioField = exports.RadioField = withOptions(Option, function (_ref3) {
-  var button = _ref3.button;
+var RadioField = exports.RadioField = withOptions(null, function (_ref2) {
+  var button = _ref2.button;
   return button ? RadioButton : _antd.Radio;
 })(RadioGroup);
-var SelectField = exports.SelectField = withOptions(Option)(_select2.default);
+var SelectField = exports.SelectField = withOptions(Option)(_antd.Select);
