cb65442d28dec5f08a083d7f14b1a1cb5becb5b7
Ilya Kil <ilya.kil@mll.com>
2019-09-16 10:03:28 +0000

    Use original event https://github.com/zmitry/redux-form-antd/issues/75

--- src/maps/textFieldMap.js
+++ src/maps/textFieldMap.js
@@ -2,5 +2,5 @@ import { customMap } from "./mapError";
 
 export default customMap((mapProps, { input: { onChange } }) => ({
   ...mapProps,
-  onChange: v => onChange(v.nativeEvent.target.value)
+  onChange: v => onChange(v.target.value)
 }));
